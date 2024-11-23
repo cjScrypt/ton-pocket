574889f63e14630ac27fae69089a578fbd94c88a
Electrifier <samchijioke16@gmail.com>
2024-11-23 15:10:43 +0100

    Updated 'pocket wallet' contract

--- contracts/pocket_wallet.ts
+++ contracts/pocket_wallet.ts
@@ -1,12 +1,12 @@
 import { Cell, beginCell, Address } from "@ton/ton";
 
 
-export function data(params: { spendableBalance: bigint, ownerAddress: Address, jettonMasterAddress: Address, jettonWalletCode: Cell, goalPocketCode: Cell, goalBalance: bigint }): Cell {
+export function data(params: { spendableBalance: bigint, ownerAddress: Address, mintAddress: Address, pocketWalletCode: Cell, goalPocketCode: Cell, goalBalance: bigint }): Cell {
     return beginCell()
         .storeCoins(params.spendableBalance)
         .storeAddress(params.ownerAddress)
-        .storeAddress(params.jettonMasterAddress)
-        .storeRef(params.jettonWalletCode)
+        .storeAddress(params.mintAddress)
+        .storeRef(params.pocketWalletCode)
         .storeRef(params.goalPocketCode)
         .storeCoins(params.goalBalance)
     .endCell();
